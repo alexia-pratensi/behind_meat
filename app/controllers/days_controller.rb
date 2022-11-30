774933c301c0e052d02c79a324d4602f1029c55a
alexia pratensi <alexiapratensi@hotmail.fr>
2022-11-30 23:00:40 +0100

    debut new-create

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,2 +1,39 @@
 class DaysController < ApplicationController
+
+  def show
+    @day = Day.find(params[:id])
+  end
+
+  def new
+    @day = Day.new
+  end
+
+  def create
+    @day = Day.new(day_params)
+    @conso.day_id = @conso
+    @conso = Conso.new(conso_params)
+    @conso.meat_id = @meat
+    @meat = Meat.new(meat_params)
+
+    if @day.save && @conso.save && @meat.save
+      redirect_to day_path(@day)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def day_params
+    params.require(:day).permit(:date)
+  end
+
+  def conso_params
+    params.require(:conso).permit(:quantity)
+  end
+
+  def meat_params
+    params.require(:meat).permit(:type)
+  end
+
 end
