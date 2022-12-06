36a152cf5c959db857fde9fe107acfc0cb55cc6b
Melanie Armitano <melanie.armi@gmail.com>
2022-12-06 17:42:33 +0100

    save(

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,21 +1,15 @@
 // Entry point for the build script in your package.json
 // import "@hotwired/turbo-rails"
 import "./controllers"
-// import "bootstrap"
-
-// export default class extends Controller {
-//   connect() {
-//     donut();
-//   }
-// }
-// console.log('appli');
 
 import {meatQuantity} from './components/meat_quantity'
 import { donut } from "./components/charts";
+
 import { animalsDeath } from "./components/animals_death";
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
+import { donuttwo } from "./components/chartstwo";
 
 Rails.start();
 Turbolinks.start();
@@ -24,5 +18,6 @@ ActiveStorage.start();
 document.addEventListener("turbolinks:load", function () {
   meatQuantity()
   donut()
+  donuttwo()
   // animalsDeath()
 });
