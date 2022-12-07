8f8fddfa53dcb3e87515ed5375e6930a528b47e7
Melanie Armitano <melanie.armi@gmail.com>
2022-12-07 09:11:07 +0100

    réglages donut

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,31 +15,31 @@ class PagesController < ApplicationController
     # @days.where(user.id == current_user.id)
 
     @beef_total_conso = @consos.where(meat_id: Meat.find_by_meat_type('boeuf')).pluck(:quantity).sum
-    @user_total_beef_conso = @consos.pluck(:quantity).sum
-    @beef_percent = (@beef_total_conso.to_f / @user_total_beef_conso.to_f * 100).round(2)
+    @user_total_conso = @consos.pluck(:quantity).sum
+    @beef_percent = (@beef_total_conso.to_f / @user_total_conso.to_f * 100).round(2)
 
     # Consommation de poulet
 
     @chicken_total_conso = @consos.where(meat_id: Meat.find_by_meat_type('poulet')).pluck(:quantity).sum
-    @user_total_beef_conso = @consos.pluck(:quantity).sum
-    @chicken_percent = (@chicken_total_conso.to_f / @user_total_beef_conso.to_f * 100).round(2)
+    @user_total_conso = @consos.pluck(:quantity).sum
+    @chicken_percent = (@chicken_total_conso.to_f / @user_total_conso.to_f * 100).round(2)
 
     # Consommation de porc
 
     @pork_total_conso = @consos.where(meat_id: Meat.find_by_meat_type('porc')).pluck(:quantity).sum
-    @user_total_beef_conso = @consos.pluck(:quantity).sum
-    @pork_percent = (@pork_total_conso.to_f / @user_total_beef_conso.to_f * 100).round(2)
+    @user_total_conso = @consos.pluck(:quantity).sum
+    @pork_percent = (@pork_total_conso.to_f / @user_total_conso.to_f * 100).round(2)
 
     # Consommation de mouton
 
-    @sheep_total_conso = @consos.where(meat_id: Meat.find_by_meat_type('sheep')).pluck(:quantity).sum
-    @user_total_beef_conso = @consos.pluck(:quantity).sum
-    @sheep_percent = (@sheep_total_conso.to_f / @user_total_beef_conso.to_f * 100).round(2)
+    @sheep_total_conso = @consos.where(meat_id: Meat.find_by_meat_type('mouton')).pluck(:quantity).sum
+    @user_total_conso = @consos.pluck(:quantity).sum
+    @sheep_percent = (@sheep_total_conso.to_f / @user_total_conso.to_f * 100).round(2)
 
     # Consommation de poisson
 
-    @fish_total_conso = @consos.where(meat_id: Meat.find_by_meat_type('fish')).pluck(:quantity).sum
-    @user_total_beef_conso = @consos.pluck(:quantity).sum
-    @fish_percent = (@fish_total_conso.to_f / @user_total_beef_conso.to_f * 100).round(2)
+    @fish_total_conso = @consos.where(meat_id: Meat.find_by_meat_type('poisson')).pluck(:quantity).sum
+    @user_total_conso = @consos.pluck(:quantity).sum
+    @fish_percent = (@fish_total_conso.to_f / @user_total_conso.to_f * 100).round(2)
   end
 end
